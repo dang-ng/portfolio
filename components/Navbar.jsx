e28abe12d5aa46343cfd7774dcf2187612d1439a
derek089 <nguyen.hai.dang@icloud.com>
2022-09-10 08:04:24 +0700

    2nd commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,51 +1,91 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from "react-icons/ai";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
-import {BsFillPersonLinesFill} from 'react-icons/bs'
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
+  const [shadow, setShadow] = useState(false)
+  const [navBg, setNavBg] = useState('#ecf0f3')
+  const [linkColor, setLinkColor] = useState('#1f2937')
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.asPath === '/urlShortener' ||
+        router.asPath === '/crypto' 
+    ) {
+      setNavBg('transparent')
+      setLinkColor('#ecf0f3')
+    } else {
+      setNavBg('#ecf0f3')
+      setLinkColor('#1f2937')
+    }
+},[router])
+
   const handleNav = () => {
     setNav(!nav)
   }
+
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+      setShadow(true)
+      } else {
+        setShadow(false)
+    }
+    }
+    window.addEventListener('scroll', handleShadow)
+},[])
+
     return (
-      <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div style={{backgroundColor: `${navBg}`}}
+        className={
+          shadow
+            ? "fixed w-full h-20 shadow-xl z-[100]"
+            : "fixed w-full h-20 z-[100]"
+        }
+      >
         <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-          <Image
+          <Link href='/' >
+            <Image
+              className='cursor-pointer'
             src="/../public/assets/navLogo.png"
             alt="/"
-            height="125"
-            width="50"
+            height="100"
+            width="100"
           />
+            
+          </Link>
           <div>
-            <ul className="hidden md:flex">
+            <ul style={{color: `${linkColor}`}} className="hidden md:flex">
               <Link href="/">
                 <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
               </Link>
-              <Link href="/">
+              <Link href="/#about">
                 <li className="ml-10 text-sm uppercase hover:border-b">
                   About
                 </li>
               </Link>
-              <Link href="/">
+              <Link href="/#skills">
                 <li className="ml-10 text-sm uppercase hover:border-b">
                   Skills
                 </li>
               </Link>
-              <Link href="/">
+              <Link href="/#projects">
                 <li className="ml-10 text-sm uppercase hover:border-b">
                   Projects
                 </li>
               </Link>
-              <Link href="/">
+              <Link href="/#contact">
                 <li className="ml-10 text-sm uppercase hover:border-b">
                   Contact
                 </li>
               </Link>
             </ul>
-            <div onClick={handleNav} className="md:hidden">
+            <div onClick={handleNav} className="md:hidden cursor-pointer">
               <AiOutlineMenu size={25} />
             </div>
           </div>
@@ -67,12 +107,14 @@ const Navbar = () => {
           >
             <div>
               <div className="flex w-full items-center justify-between">
-                <Image
+                <Link href='/'>
+                <Image className='cursor-pointer'
                   src="/../public/assets/navLogo.png"
                   width="87"
-                  height="35"
+                  height="87"
                   alt="/"
                 />
+                </Link>
                 <div
                   onClick={handleNav}
                   className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
@@ -89,19 +131,19 @@ const Navbar = () => {
             <div className="py-4 flex flex-col">
               <ul className="uppercase">
                 <Link href="/">
-                  <li className="py-4 text-sm">Home</li>
+                  <li /*onClick={()=>setNav(false)}*/ className="py-4 text-sm">Home</li>
                 </Link>
-                <Link href="/">
-                  <li className="py-4 text-sm">About</li>
+                <Link href="/#about">
+                  <li /*onClick={()=>setNav(false)}*/ className="py-4 text-sm">About</li>
                 </Link>
-                <Link href="/">
-                  <li className="py-4 text-sm">Skills</li>
+                <Link href="/#skills">
+                  <li /*onClick={()=>setNav(false)}*/ className="py-4 text-sm">Skills</li>
                 </Link>
-                <Link href="/">
-                  <li className="py-4 text-sm">Projects</li>
+                <Link href="/#projects">
+                  <li /*onClick={()=>setNav(false)}*/ className="py-4 text-sm">Projects</li>
                 </Link>
-                <Link href="/">
-                  <li className="py-4 text-sm">Contact</li>
+                <Link href="/#contact">
+                  <li /*onClick={()=>setNav(false)}*/ className="py-4 text-sm">Contact</li>
                 </Link>
               </ul>
               <div className="pt-40">
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import PropertyImg from '../public/assets/projects/property.jpg'
+import UrlShortener from '../public/assets/projects/property.jpg'
 import CryptoImg from '../public/assets/projects/crypto.jpg'
 import NetflixIgm from '../public/assets/projects/netflix.jpg'
 import TwitchIgm from '../public/assets/projects/twitch.jpg'
@@ -9,7 +9,7 @@ import ProjectItems from './ProjectItems'
 
 const Projects = () => {
   return (
-    <div className="w-full">
+    <div id="projects" className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Projects
@@ -17,9 +17,9 @@ const Projects = () => {
         <h2 className="py-4">What I Have Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
           <ProjectItems
-            title="Property Finder"
-                      backgroundImg={PropertyImg}
-            projectUrl="/property"
+            title="URL shortener and File Sharing"
+                      backgroundImg={UrlShortener}
+            projectUrl="/urlShortener"
           />
           <ProjectItems
             title="Crypto App"

495e295ac661912e735cd27be54b7a46d9d2620f
derek089 <nguyen.hai.dang@icloud.com>
2022-09-15 19:57:55 +0700

    auto close

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -133,27 +133,27 @@ const Navbar = () => {
             <div className="py-4 flex flex-col">
               <ul className="uppercase">
                 <Link href="/">
-                  <li /*onClick={()=>setNav(false)}*/ className="py-4 text-sm">
+                  <li onClick={()=>setNav(false)} className="py-4 text-sm">
                     Home
                   </li>
                 </Link>
                 <Link href="/#about">
-                  <li /*onClick={()=>setNav(false)}*/ className="py-4 text-sm">
+                  <li onClick={()=>setNav(false)} className="py-4 text-sm">
                     About
                   </li>
                 </Link>
                 <Link href="/#skills">
-                  <li /*onClick={()=>setNav(false)}*/ className="py-4 text-sm">
+                  <li onClick={()=>setNav(false)} className="py-4 text-sm">
                     Skills
                   </li>
                 </Link>
                 <Link href="/#projects">
-                  <li /*onClick={()=>setNav(false)}*/ className="py-4 text-sm">
+                  <li onClick={()=>setNav(false)} className="py-4 text-sm">
                     Projects
                   </li>
                 </Link>
                 <Link href="/#contact">
-                  <li /*onClick={()=>setNav(false)}*/ className="py-4 text-sm">
+                  <li onClick={()=>setNav(false)} className="py-4 text-sm">
                     Contact
                   </li>
                 </Link>
